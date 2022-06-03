bb9db84cad1292dcf9d62b1c18e467fcc830bc77
Phillip Marques <phillip.silva@capgemini.com>
2022-06-03 19:49:26 -0300

    Single page application 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -16,6 +16,10 @@ export const routes: Routes =[
         component: MenuComponent
     },
     {
+        path: 'dishdetail/:id',
+        component: DishdetailComponent
+    },
+    {
         path: 'contact',
         component: ContactComponent
     },
