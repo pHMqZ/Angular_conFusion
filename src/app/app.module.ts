b729416a4c5c698c152c6baee9729d5e0f4025ff
Phillip Marques <79226575+pHMqZ@users.noreply.github.com>
2022-06-09 18:43:21 -0300

    Promises (#22)
    * Dialog
    
    * Template forms 1
    
    * Template forms 2
    
    * Reactive Forms 1
    
    * Reactive Forms 2
    
    * Promises  (#21)
    
    * Promises 1
    
    * Promises 2
    
    Co-authored-by: Phillip Marques

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms'; 
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
@@ -64,6 +64,8 @@ import { LoginComponent } from './login/login.component';
     FormsModule,
     ReactiveFormsModule,
     MatSelectModule,
+    MatSlideToggleModule,
+    MatProgressSpinnerModule
     MatSlideToggleModule
   ],
   providers: [
