eeb0e4fe31c77932d3eaf8ed3e2b7299239f4c00
Phillip Marques <phillip.silva@capgemini.com>
2022-06-10 14:58:05 -0300

    RxJs 1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -14,7 +14,7 @@ export class AboutComponent implements OnInit {
   constructor(private leaderService: LeaderService) { }
 
   ngOnInit(): void {
-    this.leaderService.getLeaders().then(leader => this.leader=leader);
+    this.leaderService.getLeaders().subscribe(leader => this.leader=leader);
   }
 
 }
