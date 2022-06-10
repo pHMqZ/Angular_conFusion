2e7053fdfe33f33adb614a14bd5e92b4819bdb9f
Phillip Marques <phillip.silva@capgemini.com>
2022-06-10 19:26:22 -0300

    Assignment 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule } from '@angular/material/slider';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
@@ -66,7 +67,8 @@ import { LoginComponent } from './login/login.component';
     MatSelectModule,
     MatSlideToggleModule,
     MatProgressSpinnerModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatSliderModule
   ],
   providers: [
     DishService,
--- src/app/shared/comment.ts
+++ src/app/shared/comment.ts
@@ -1,4 +1,4 @@
-export class Comment{
+export class Comment {
     rating!: number;
     comment!: string;
     author!: string;
