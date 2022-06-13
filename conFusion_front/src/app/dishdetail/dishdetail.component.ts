425f235a751d02416c17546f49f2a4518d13581a
Phillip Marques <phillip.silva@capgemini.com>
2022-06-13 17:19:11 -0300

    Saving Changes to Server

--- conFusion_front/src/app/dishdetail/dishdetail.component.ts
+++ conFusion_front/src/app/dishdetail/dishdetail.component.ts
@@ -22,6 +22,7 @@ export class DishdetailComponent implements OnInit {
   @ViewChild('cform') commentFormDirective;
   commentForm: FormGroup;
   comment: Comment;
+  dishcopy: Dish;
 
   formErrors = {
     'author': '',
@@ -51,7 +52,8 @@ export class DishdetailComponent implements OnInit {
     this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params.pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
       .subscribe(dish => {
-        this.dish = dish;
+        this.dish = dish; 
+        this.dishcopy = dish;
         this.setPrevNext(dish.id);
       }, errmess => this.errMess = <any>errmess);
   }
@@ -93,7 +95,10 @@ export class DishdetailComponent implements OnInit {
   onSubmit() {
     this.comment = this.commentForm.value;
     this.comment.date = new Date().toISOString();
-    this.dish.comments.push(this.comment);
+    this.dishcopy.comments.push(this.comment);
+    this.dishService.putDish(this.dishcopy).subscribe(dish => {
+      this.dish = dish; this.dishcopy =dish;
+    }, errmess => {this.dish = null; this.dishcopy = null; this.errMess=<any>errmess});
     console.log(this.comment);
     this.commentForm.reset({
       rating: '5',
