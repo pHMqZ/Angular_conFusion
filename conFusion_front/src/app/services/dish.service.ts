99b889e971794cc7394f6bff3f8e97d9ec498f0e
Phillip Marques <79226575+pHMqZ@users.noreply.github.com>
2022-06-13 17:00:28 -0300

    Week4 (#29)
    * Http 1
    
    * Configs json-server
    
    * Week4 http (#28)

--- conFusion_front/src/app/services/dish.service.ts
+++ conFusion_front/src/app/services/dish.service.ts
@@ -1,34 +1,36 @@
 import {Component, OnInit, Inject, Injectable} from '@angular/core';
 import {Dish} from '../shared/dish';
-// import {DISHES} from '../shared/dishes';
-// import {delay} from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { baseURL } from '../shared/baseurl';
+import { ProcessHTTPMsgService } from './process-httpmsg.service';
 import { map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { baseURL } from '../shared/baseurl';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class DishService {
 
-  constructor(private http: HttpClient) {
+
+  constructor(private http: HttpClient, private processHTTPMsgService: ProcessHTTPMsgService) {
   }
 
   getDishes(): Observable<Dish[]> {
-    return this.http.get<Dish[]>(baseURL + 'dishes');
+    return this.http.get<Dish[]>(baseURL + 'dishes').pipe(catchError(this.processHTTPMsgService.handleError));
   }
 
   getDish(id: string): Observable<Dish> {
-    return this.http.get<Dish>(baseURL + 'dishes/' + id);
+    return this.http.get<Dish>(baseURL + 'dishes/' + id).pipe(catchError(this.processHTTPMsgService.handleError));
   }
 
   getFeaturedDish(): Observable<Dish> {
-    return this.http.get<Dish[]>(baseURL + 'dishes?featured=true').pipe(map(dishes => dishes[0]));
+    return this.http.get<Dish[]>(baseURL + 'dishes?featured=true').pipe(map(dishes => dishes[0])).pipe(catchError(this.processHTTPMsgService.handleError));
   }
 
   getDishIds(): Observable<string[] | any> {
-    return this.getDishes().pipe(map(dishes => dishes.map(dish => dish.id)));
+    return this.getDishes().pipe(map(dishes => dishes.map(dish => dish.id))).pipe(catchError(this.processHTTPMsgService.handleError));
   }
 }
