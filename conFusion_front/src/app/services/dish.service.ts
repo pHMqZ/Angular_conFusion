7be6104e3fd6bf5c4f610b952a01f43f92ce15bc
Phillip Marques <phillip.silva@capgemini.com>
2022-06-13 16:20:58 -0300

    Http 1

---
+++ conFusion_front/src/app/services/dish.service.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit, Inject, Injectable} from '@angular/core';
+import {Dish} from '../shared/dish';
+// import {DISHES} from '../shared/dishes';
+// import {delay} from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { baseURL } from '../shared/baseurl';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DishService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getDishes(): Observable<Dish[]> {
+    return this.http.get<Dish[]>(baseURL + 'dishes');
+  }
+
+  getDish(id: string): Observable<Dish> {
+    return this.http.get<Dish>(baseURL + 'dishes/' + id);
+  }
+
+  getFeaturedDish(): Observable<Dish> {
+    return this.http.get<Dish[]>(baseURL + 'dishes?featured=true').pipe(map(dishes => dishes[0]));
+  }
+
+  getDishIds(): Observable<string[] | any> {
+    return this.getDishes().pipe(map(dishes => dishes.map(dish => dish.id)));
+  }
+}
