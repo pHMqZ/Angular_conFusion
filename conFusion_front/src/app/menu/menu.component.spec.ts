eac9c855a8acf1a431cd323ecb23232590bceb7e
Phillip Marques <79226575+pHMqZ@users.noreply.github.com>
2022-06-15 19:01:07 -0300

    Finished project (#36)
    * Http 1
    
    * Configs json-server
    
    * Week4 http (#28)
    
    * Http 1 (#26)
    
    * Config json-server (#27)
    
    * Http 2
    
    * Saving Changes to Server
    
    * Assignment 4 - HTTP
    
    * Directives
    
    * Animations Part 1
    
    * Animations Part 2
    
    * Assignment Part 2
    
    * add spinner in contact component
    
    * Component test
    
    * End-to-End Test
    
    * Finished project

--- conFusion_front/src/app/menu/menu.component.spec.ts
+++ conFusion_front/src/app/menu/menu.component.spec.ts
@@ -1,16 +1,58 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatGridListModule, MatProgressSpinnerModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Observable, of } from 'rxjs';
+import { Dish } from '../shared/dish';
+import { DISHES } from '../shared/dishes';
 
 import { MenuComponent } from './menu.component';
+import { DishService } from '../services/dish.service';
+import { baseURL } from '../shared/baseurl';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
   beforeEach(async(() => {
+
+    const dishServiceStub = {
+      getDishes: function(): Observable<Dish[]> {
+        return of (DISHES);
+      }
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+      imports: [
+        BrowserAnimationsModule,
+        FlexLayoutModule,
+        MatGridListModule,
+        MatProgressSpinnerModule,
+        RouterTestingModule.withRoutes([{
+          path: 'menu',
+          component: MenuComponent
+        }])
+      ],
+      declarations: [ 
+        MenuComponent 
+      ],
+      providers: [
+        { 
+          provide: DishService, 
+          useValue: dishServiceStub 
+        },
+        { 
+          provide: 'baseURL', 
+          useValue: baseURL 
+        },
+      ]
     })
     .compileComponents();
+
+    const dishservice = TestBed.get(DishService);
   }));
 
   beforeEach(() => {
@@ -21,5 +63,22 @@ describe('MenuComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('dishes items should be 4', () => {
+    expect(component.dishes.length).toBe(4);
+    expect(component.dishes[1].name).toBe('Zucchipakoda');
+    expect(component.dishes[3].featured).toBeFalsy();
+  });
+
+  it('should use dishes in the template', () => {
+    fixture.detectChanges();
+
+    let de:      DebugElement;
+    let el:      HTMLElement;
+    de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
+
+    expect(el.textContent).toContain(DISHES[0].name.toUpperCase());
+
   });
 });
